3bddd95bf6c6c3cc8d5ce89350628b7b63587269
moinreza <contact.shahmoinreza@gmail.com>
2025-01-12 14:55:10 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+[
+    {
+        "id": 1,
+        "name": "Product 1",
+        "description": "This is product 1",
+        "price": 10.00,
+        "image": "https://via.placeholder.com/150"
+    },
+    {
+        "id": 2,
+        "name": "Product 2",
+        "description": "This is product 2",
+        "price": 20.00,
+        "image": "https://via.placeholder.com/150"
+    }
+]
